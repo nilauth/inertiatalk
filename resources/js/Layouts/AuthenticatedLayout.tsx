18fe7574a6584e00638e25387190cd65b416ab43
nilauth <nilauth.dev@gmail.com>
2024-08-11 22:30:11 +0100

    Replace HeadlessUI dropdown with Shadcn dropdown component

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,10 +1,18 @@
 import { PropsWithChildren, useEffect, useState } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import { User } from '@/types';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ChevronDown } from 'lucide-react';
 
 export default function Authenticated({
     children,
@@ -12,13 +20,11 @@ export default function Authenticated({
 }: PropsWithChildren<{
     user: User;
 }>) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(true);
 
     useEffect(() => {
         console.log('auth layout mounted');
     }, []);
-
-    console.log(showingNavigationDropdown);
 
     return (
         <div className="min-h-screen w-full bg-gray-100 dark:bg-gray-900">
@@ -41,38 +47,74 @@ export default function Authenticated({
 
                         <div className="hidden sm:ms-6 sm:flex sm:items-center">
                             <div className="relative ms-3">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
+                                {/*<Dropdown>*/}
+                                {/*    <Dropdown.Trigger>*/}
+                                {/*        <span className="inline-flex rounded-md">*/}
+                                {/*            <button*/}
+                                {/*                type="button"*/}
+                                {/*                className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300"*/}
+                                {/*            >*/}
+                                {/*                {user.name}*/}
+
+                                {/*                <svg*/}
+                                {/*                    className="-me-0.5 ms-2 h-4 w-4"*/}
+                                {/*                    xmlns="http://www.w3.org/2000/svg"*/}
+                                {/*                    viewBox="0 0 20 20"*/}
+                                {/*                    fill="currentColor"*/}
+                                {/*                >*/}
+                                {/*                    <path*/}
+                                {/*                        fillRule="evenodd"*/}
+                                {/*                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"*/}
+                                {/*                        clipRule="evenodd"*/}
+                                {/*                    />*/}
+                                {/*                </svg>*/}
+                                {/*            </button>*/}
+                                {/*        </span>*/}
+                                {/*    </Dropdown.Trigger>*/}
+
+                                {/*    <Dropdown.Content>*/}
+                                {/*        <Dropdown.Link*/}
+                                {/*            onClick={() => console.log(showingNavigationDropdown)}*/}
+                                {/*            href={route('profile.edit')}*/}
+                                {/*        >*/}
+                                {/*            Profile*/}
+                                {/*        </Dropdown.Link>*/}
+                                {/*        <Dropdown.Link href={route('logout')} method="post" as="button">*/}
+                                {/*            Log Out*/}
+                                {/*        </Dropdown.Link>*/}
+                                {/*    </Dropdown.Content>*/}
+                                {/*</Dropdown>*/}
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <div className="ml-2 flex items-center justify-center text-sm">
+                                            {user.name} <ChevronDown className="h-4 w-4" />
+                                        </div>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem asChild>
+                                            <Link
+                                                method="get"
+                                                className="h-full w-full text-left"
+                                                href={route('profile.edit')}
+                                                as="button"
                                             >
-                                                {user.name}
-
-                                                <svg
-                                                    className="-me-0.5 ms-2 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
+                                                Profile
+                                            </Link>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem asChild>
+                                            <Link
+                                                method="post"
+                                                className="w-full text-left"
+                                                href={route('logout')}
+                                                as="button"
+                                            >
+                                                Log out
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
                             </div>
                         </div>
 
